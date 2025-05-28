842d9efb5d8cff978b6a1f87f0e28fc5aec7df97
Mikolaj Kluk <mikolaj.kluk@wp.pl>
2025-05-28 16:42:33 +0200

    Authentication with Auth.js added

--- adventurers/models.py
+++ adventurers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Adventurer(models.Model):
+    name = models.CharField(max_length=100)
+    race = models.CharField(max_length=50)
+    character_class = models.CharField(max_length=50)
+    level = models.PositiveIntegerField(default=1)
+
+    strength = models.IntegerField(default=10)
+    dexterity = models.IntegerField(default=10)
+    constitution = models.IntegerField(default=10)
+    intelligence = models.IntegerField(default=10)
+    wisdom = models.IntegerField(default=10)
+    charisma = models.IntegerField(default=10)
+
+    background = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} the {self.race} {self.character_class} (Lvl {self.level})"
--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from .models import Adventurer
 
 # Register your models here.
-
-admin.site.register(Adventurer)
